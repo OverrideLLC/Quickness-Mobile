58e860f690149e2def077585e6e1a9e1d0936e0d
Christopher <130204738+BeboCod@users.noreply.github.com>
2024-12-22 10:39:13 -0600

    feat: Implement initial structure for iOS platform
    - Added initial implementation for FirebaseService, GoogleMaps, Uri, and SharedPreference for iOS.
    - Created NativeModule with Darwin dependency for iOS.
    - All functions are currently placeholders (TODOs).

--- composeApp/src/iosMain/kotlin/org/quickness/data/service/FirebaseService.kt
+++ composeApp/src/iosMain/kotlin/org/quickness/data/service/FirebaseService.kt
@@ -1,5 +1,60 @@
 package org.quickness.data.service
 
+import org.quickness.data.Result.AuthResult
+import org.quickness.data.Result.ForgotPasswordResult
 import org.quickness.interfaces.plataform.FirebaseAuth
 
-actual class FirebaseService : FirebaseAuth
+actual class FirebaseService : FirebaseAuth {
+    actual override suspend fun signIn(
+        email: String,
+        password: String
+    ): AuthResult {
+        TODO("Not yet implemented")
+    }
+
+    actual override suspend fun forgotPassword(email: String): ForgotPasswordResult? {
+        TODO("Not yet implemented")
+    }
+
+    actual override suspend fun reauthenticateAndChangePassword(
+        email: String,
+        currentPassword: String,
+        newPassword: String,
+        onSuccess: () -> Unit,
+        onError: (Exception) -> Unit
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    actual override fun changePassword(
+        newPassword: String,
+        onSuccess: () -> Unit,
+        onError: (Exception) -> Unit
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    actual override fun changeEmail(
+        newEmail: String,
+        onSuccess: () -> Unit,
+        onError: (Exception) -> Unit
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    actual override suspend fun logOut(
+        onSuccess: () -> Unit,
+        onError: (Exception) -> Unit
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    actual override suspend fun reauthenticate(
+        email: String,
+        currentPassword: String,
+        onSuccess: () -> Unit,
+        onError: (Exception) -> Unit
+    ) {
+        TODO("Not yet implemented")
+    }
+}
